5aee86e07d880c1854909af73362690cd83b7c79
Michael-Boat-2 <michael.anokyeboateng@gmail.com>
2026-07-05 15:14:41 +0200

    Name Input Management

---
+++ Assets/Scripts/Managers/NameInputManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+namespace Managers
+{
+    public class NameInputManager : MonoBehaviour
+    {
+        
+        [SerializeField] private PlayerProfileData profileData;
+        [SerializeField] private TMP_InputField nameInput;
+        [SerializeField] private TMP_Text errorText;
+        
+        
+        private void Start()
+        {
+            if (!profileData)
+                Debug.LogError("No profile data found");
+        }
+        
+        public void OnConfirm()
+        {
+            if (profileData && !string.IsNullOrWhiteSpace(nameInput.text))
+            {
+                profileData.playerName = nameInput.text.Trim();
+                SceneManager.LoadScene("Onboarding");
+            }
+            else
+            {
+                if (errorText)
+                {
+                    StopAllCoroutines();
+                    StartCoroutine(ShowError());
+                }
+               
+            }
+        }
+
+        private IEnumerator ShowError()
+        {
+            errorText.gameObject.SetActive(true);
+            errorText.text = "You entered invalid name";
+
+            yield return new WaitForSeconds(2f);
+            
+            errorText.gameObject.SetActive(false);
+        }
+        
+       
+    }
+}
